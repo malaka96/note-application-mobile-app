abdb105364a080e0546e421f21a2a3db168b9315
malaka96 <madhubhashana655@gmail.com>
2025-12-31 00:13:48 +0530

    completed fetch all notes from api

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:note_application_mobile_app/services/api_services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Page"),
+      appBar: AppBar(title: Text("Home Page")),
+      body: FutureBuilder(
+        future: ApiServices().fetchAllNotes(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(child: Text("Error fetching notes"));
+          } else if (!snapshot.hasData) {
+            return const Center(child: Text("No any notes avaliable"));
+          } else {
+            final notes = snapshot.data!;
+            return ListView.builder(
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                final note = notes[index];
+                return Text(note.title);
+              },
+            );
+          }
+        },
       ),
     );
   }
---
+++ lib/services/api_services.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:note_application_mobile_app/models/note.dart';
+import 'package:http/http.dart' as http;
+
+class ApiServices {
+  Future<List<Note>> fetchAllNotes() async {
+    final response = await http.get(Uri.parse("http://localhost:8080/"));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body);
+
+      return jsonData.map((item) {
+        return Note.fromJson(item);
+      }).toList();
+    } else {
+      throw Exception("Failed to fetch all notes");
+    }
+  }
+}
